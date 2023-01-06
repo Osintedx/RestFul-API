eb8eadd03a42f4cc2a4403eb43c51a30d4b1e161
Barcode <88603393+Actuallybarcode@users.noreply.github.com>
2023-01-06 07:01:46 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+import usersRoutes from './routes/users.js';
+
+const app = express();
+const PORT = 5000;
+
+app.use(bodyParser.json());
+
+app.use('/users', usersRoutes);
+
+app.get('/', (req, res) => res.send('Hello lol'));
+
+app.listen(PORT, () => console.log(`Server Running on port: http://localhost:${PORT}`));
